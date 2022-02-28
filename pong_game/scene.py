8efc0761fdd23f94d7944071299601e703b196e8
moura <m0ur4@protonmail.com>
2022-02-28 17:37:18 -0300

    Changelog:
    * Re-organised the pygame imports. More specific now.
    * Fixed typos in the Scene class.
    * Added missing documentation to the Scene class.
    
    MainMenuScene Class Changelog:
    
    * Created a function instead of using a lambda to trigger the play_button.
    * Created a function instead of using a lambda to trigger the settings_button.
    * Refactored the code to suit the new changes of the Button class (interface.py)
    
    GameScene Class Changelog:
    
    * Update class description.
    * Game whole implementation finished.
    * Added basic interface.
    * Added countdown timer.
    * Code responsible for setting up the interface and game elements were extracted
      into new methods (setup_game_elements() and setup_interface_elements()).
    * Code responsible for updating the game and the countdown timer were extracted into new methods (update_game_elements() and countdown_handling())
    * A-key and D-key are the new paddle controllers.

--- pong_game/scene.py
+++ pong_game/scene.py
@@ -1,10 +1,15 @@
-import pygame
-
-from .game_elements.ball import Ball
-
-from .game_elements.paddle import Paddle
+import pygame.constants as constants
+import pygame.draw as draw
+import pygame.rect as rect
+import pygame.surface as surface
+import pygame.time as time
 
 from . import effects, interface, utils
+from .game_elements.ball import Ball
+from .game_elements.paddle import Paddle
+from .game_elements.targets import Targets
+
+# TODO: Create a settings view
 
 
 class Scene:
@@ -28,14 +33,25 @@ class Scene:
 
     def draw(self):
         """It draws the components of this scene in the screen."""
+
         pass
 
     def update(self):
         """It updates the components everytime in the loop."""
+
         pass
 
     def update_on_event(self, event):
-        """It updates the components if a event occur."""
+        """It updates the components if a event occur.
+        
+        Args:
+        
+            event:
+                This is an object passed by the iteration, that is
+                responsible for iterating all events
+                (pygame.event.get()).
+        """
+
         pass
 
 
@@ -47,47 +63,45 @@ class DebugScene(Scene):
     def __init__(self, screen):
         super().__init__(screen)
 
-        self.bg = pygame.Surface(self.screen.get_size())
+        self.bg = surface.Surface(self.screen.get_size())
         self.bg.fill((200, 200, 200))
 
         self.rect = self.bg.get_rect()
         self.rect.center = self.screen_rect.center
 
         # Do whatever you want here
-        self.ball = Ball(screen)
-        self.ball.pos = [300, 200]
+        self.test_rect = rect.Rect(0, 0, 75, 30)
+        self.test_rect.center = self.screen_rect.center
 
     def draw(self):
         self.screen.blit(self.bg, self.rect)
-        self.ball.draw()
+        draw.rect(self.screen, (0, 0, 180), self.test_rect)
 
-    def update_on_event(self, event):
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                self.ball.pos[1] -= 100
-            elif event.key == pygame.K_DOWN:
-                self.ball.pos[1] += 100
+
+# TODO: Find a safer way of timing stuff. Sending events is proper to
+# fail
 
 
 class IntroScene(Scene):
     """A scene that shows the logo of the creator of the game."""
 
-    END_INTRO = pygame.USEREVENT + 1
+    END_INTRO = constants.USEREVENT + 1
 
     def __init__(self, screen):
         super().__init__(screen)
 
-        self.logo_icon = interface.Label.from_image(
+        self.logo_icon = interface.Label(
             screen, utils.load_image("game_intro/moura_cat.png"))
-        self.logo_title = interface.Label.from_image(
+        self.logo_title = interface.Label(
             screen, utils.load_image("game_intro/logo_title.png"))
 
         self.logo_icon.rect.centerx = self.screen_rect.centerx
         self.logo_icon.rect.centery = self.screen_rect.centery
+
         self.logo_title.rect.centerx = self.screen_rect.centerx
         self.logo_title.rect.centery = self.screen_rect.centery + 74
 
-        pygame.time.set_timer(IntroScene.END_INTRO, 3000, 1)
+        time.set_timer(IntroScene.END_INTRO, 3000, 1)
 
     def draw(self):
         self.screen.fill((255, 255, 255))
@@ -114,32 +128,46 @@ class MainMenuScene(Scene):
             screen, utils.load_image("main_menu/game_title.png"),
             effects.floating_animation, 120, self.screen_rect.top)
 
-        def play_button_action(): return self.scene_manager.change_view(
-            "on_game",
-            effects.FadeTransition(
-                self.screen, self.scene_manager, "on_game", (255, 255, 0)))
+        def play_button_action():
+            fadefx = effects.FadeTransition(
+                self.screen, self.scene_manager, "on_game", (255, 255, 0))
+            self.scene_manager.change_view(
+                "on_game", fadefx)
+
+            # Always get brand new game
+            self.scene_manager.views["on_game"].retry()
+
         self.play_button = interface.Button(
             screen, utils.load_image("main_menu/play_button_on.png"),
             utils.load_image("main_menu/play_button_off.png"),
             utils.load_image("main_menu/play_button_clicked.png"),
             play_button_action)
+
+        def settings_button_action():
+            pass
+
         self.settings_button = interface.Button(
             screen, utils.load_image("main_menu/settings_button_on.png"),
             utils.load_image("main_menu/settings_button_off.png"),
-            utils.load_image("main_menu/settings_button_clicked.png"))
+            utils.load_image("main_menu/settings_button_clicked.png"),
+            settings_button_action)
         self.quit_button = interface.Button(
             screen, utils.load_image("main_menu/quit_button_on.png"),
             utils.load_image("main_menu/quit_button_off.png"),
             utils.load_image("main_menu/quit_button_clicked.png"))
 
         padding = 10
+
         self.game_title.rect.centerx = self.screen_rect.centerx
         self.game_title.rect.top = self.screen_rect.top + padding
+
         self.play_button.rect.midleft = self.screen_rect.midleft
         self.play_button.rect.x += padding
+
         self.settings_button.rect.midleft = self.screen_rect.midleft
         self.settings_button.rect.y += 35 + padding
         self.settings_button.rect.x += padding
+
         self.quit_button.rect.midleft = self.screen_rect.midleft
         self.quit_button.rect.y += 80 + padding
         self.quit_button.rect.x += padding
@@ -153,52 +181,200 @@ class MainMenuScene(Scene):
 
     def update(self):
         self.game_title.update()
+        self.play_button.update()
+        self.settings_button.update()
+        self.quit_button.update()
 
     def update_on_event(self, event):
-        self.play_button.update(event)
-        self.settings_button.update(event)
-        self.quit_button.update(event)
+        self.play_button.update_on_event(event)
+        self.settings_button.update_on_event(event)
+        self.quit_button.update_on_event(event)
 
 
 class GameScene(Scene):
-    """Shows the current game."""
+    """Scene responsible for being actually the minigame."""
 
     def __init__(self, screen):
         super().__init__(screen)
 
+        # Game flags
+        self.paused = False
+        self.game_over = False
+        self.on_countdown = True
+
+        # Timer variables
+        self.current_time = 0
+        self.countdown_tick = 0
+
+        self.attempts = 3
+
+        # Game elements
         self.ball = Ball(screen)
         self.paddle = Paddle(screen)
+        self.targets = Targets(screen)
 
+        # Interface elements
+        self.countdown_number = interface.Label.from_text(
+            screen, "1", (0, 0, 255), 36, 1, 1)
+        self.scoreboard = interface.Label.from_text(
+            screen, f"Score: {self.ball.points}", (255, 255, 255), 16, 17, 1,
+            True, antialised=True)
+        self.game_over_label = interface.Label.from_text(
+                screen, "GAME OVER", (255, 255, 255), 20, 10, 0, True,
+                antialised=True)
+        self.retry_button = interface.Button(
+            screen, utils.load_image("on_game/retry_button_on.png"),
+            utils.load_image("on_game/retry_button_off.png"),
+            utils.load_image("on_game/retry_button_clicked.png"), self.retry)
+        self.main_menu_button = interface.Button(
+            screen, utils.load_image("on_game/main_menu_button_on.png"),
+            utils.load_image("on_game/main_menu_button_off.png"),
+            utils.load_image("on_game/main_menu_button_clicked.png"),
+            lambda: self.scene_manager.change_view(
+                "main_menu",
+                effects.FadeTransition(
+                    screen, self.scene_manager, "main_menu",
+                    (255, 255, 255), 4)
+            )
+        )
+        self.paused_label = interface.Label.from_text(
+            screen, "PAUSED", (100, 0, 0), 36, 6, 0, True, antialised=True)
+
+        # Sound effects
+        self.game_over_soundfx = utils.load_soundfx(
+            "on_game/soundfx/game_over.wav")
+        self.countdown_beep_soundfx = utils.load_soundfx(
+            "on_game/soundfx/countdown_beep.wav")
+
+        self.setup_game_elements()
+        self.setup_interface_elements()
+
+    def setup_game_elements(self):
         self.ball.x, self.ball.y = self.screen_rect.center
 
         self.paddle.rect.centerx = self.screen_rect.centerx
         self.paddle.rect.centery = self.screen_rect.centery + 100
 
+    def setup_interface_elements(self):
+        self.paused_label.rect.center = self.screen_rect.center
+
+        self.game_over_label.rect.center = self.screen_rect.center
+        self.game_over_label.rect.y -= 20
+
+        self.main_menu_button.rect.center = self.screen_rect.center
+        self.main_menu_button.rect.x -= 50
+        self.main_menu_button.rect.y += 30
+
+        self.retry_button.rect.center = self.screen_rect.center
+        self.retry_button.rect.x += 50
+        self.retry_button.rect.y += 30
+
+        self.countdown_number.rect.center = self.screen_rect.center
+
+        self.scoreboard.rect.bottomright = self.screen_rect.bottomright
+
     def draw(self):
-        self.screen.fill((0, 0, 0))
-        self.ball.draw()
-        self.paddle.draw()
-    
-    def check_paddle_ball_collision(self):
-        if self.paddle.rect.collidepoint(self.ball.x, self.ball.y):
-            self.ball.yspeed *= -1
+        if self.game_over:
+            self.screen.fill((0, 20, 0))
+            self.game_over_label.draw()
+            self.main_menu_button.draw()
+            self.retry_button.draw()
+        else:
+            # Game is on
+            self.screen.fill((0, 0, 20))
+            self.targets.draw()
+            self.ball.draw()
+            self.paddle.draw()
+            self.scoreboard.draw()
+            if self.paused:
+                self.paused_label.draw()
+            elif self.on_countdown:
+                self.countdown_number.draw()
+
+    def update_game_elements(self):
+        """It updates the game related elements."""
+
+        if not (self.paused or self.on_countdown):
+            self.ball.update(self.paddle)
+            if self.ball.rect.top > self.screen_rect.bottom:
+                self.restart()
+                self.attempts -= 1
+                if self.attempts == 0:
+                    self.game_over = True
+                    self.on_countdown = False
+                    self.game_over_soundfx.play()
+            self.paddle.update()
+            self.targets.update(self.ball)
+            self.scoreboard.update_text(f"Score: {self.ball.points}")
+            self.scoreboard.rect.bottomleft = self.screen_rect.bottomleft
+
+        if self.game_over:
+            self.retry_button.update()
+            self.main_menu_button.update()
+
+    def countdown_handling(self):
+        """It's responsible for doing the game 1.. 2.. 3.. countdown
+        whenever asked to do so.
+        """
+
+        if not (self.countdown_tick or self.game_over) and self.on_countdown:
+            self.countdown_tick = time.get_ticks()
+
+            # The beep sound effect is just a sec. Better that way.
+            self.countdown_beep_soundfx.play(loops=2)
+
+        if self.on_countdown:
+            delta_time = self.current_time - self.countdown_tick
+
+            for i in range(1, 4):
+                if i*1000-1000 < delta_time < i*1000:
+                    self.countdown_number.update_text(str(i))
+                    self.countdown_number.rect.center = self.screen_rect.center
+            else:
+                if 3000 < delta_time < 3500:
+                    self.on_countdown = False
+                    self.countdown_tick = 0
 
     def update(self):
-        self.ball.update()
-        self.paddle.update()
-        self.check_paddle_ball_collision()
+        self.current_time = time.get_ticks()
+
+        self.countdown_handling()
+        self.update_game_elements()
 
     def update_on_event(self, event):
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
+        if event.type == constants.KEYDOWN:
+            if event.key == constants.K_a:
                 self.paddle.move_left(True)
-            elif event.key == pygame.K_RIGHT:
+            elif event.key == constants.K_d:
                 self.paddle.move_right(True)
-        elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT:
+            elif event.key == constants.K_p and not self.on_countdown:
+                self.paused = not self.paused
+        elif event.type == constants.KEYUP:
+            if event.key == constants.K_a:
                 self.paddle.move_left(False)
-            elif event.key == pygame.K_RIGHT:
+            elif event.key == constants.K_d:
                 self.paddle.move_right(False)
+
+        if self.game_over:
+            self.retry_button.update_on_event(event)
+            self.main_menu_button.update_on_event(event)
+
+    def restart(self):
+        """It restarts the game to its initial state."""
+
+        self.targets.recharge()
+        self.setup_game_elements()
+        self.countdown_tick = 0
+        self.on_countdown = True
+
+    def retry(self):
+        """It starts the game again."""
+
+        self.restart()
+        self.attempts = 3
+        self.countdown_tick = 0
+        self.on_countdown = True
+        self.game_over = False
 
 
 class SceneManager:
@@ -237,7 +413,7 @@ class SceneManager:
         self.views[self.current_view].draw()
         if self.on_transition:
             self.fx_object.animate()
-    
+
     def update(self):
         """It updates the components of the current scene in loop."""
 
@@ -256,7 +432,8 @@ class SceneManager:
                 event.
         """
 
-        self.views[self.current_view].update_on_event(event)
+        if not self.on_transition:
+            self.views[self.current_view].update_on_event(event)
 
     def _change_view(self, view_name):
         """It changes the current view directly."""
@@ -276,6 +453,7 @@ class SceneManager:
                 A class that is responsible for a transition of
                 views. When none, the view is changed abruptly
         """
+
         if view_name != self.current_view:
             if fx is not None:
                 self.fx_object = fx
