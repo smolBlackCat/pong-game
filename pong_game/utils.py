825b7a3fc4697d3d49aecffc3226ec4ce66e9f1e
moura <m0ur4@protonmail.com>
2022-02-28 16:50:42 -0300

    Changelog:
    * Re-organised pygame imports. More specific now.
    * Added load_soundfx() function

--- pong_game/utils.py
+++ pong_game/utils.py
@@ -2,8 +2,8 @@
 
 import os
 
-import pygame
-
+import pygame.image as image
+import pygame.mixer as mixer
 
 def load_image(path):
     """Loads the image inside the game_data directory.
@@ -15,4 +15,18 @@ def load_image(path):
             (game_data/)
     """
 
-    return pygame.image.load(os.path.join("game_data", path))
+    return image.load(os.path.join("game_data", path))
+
+
+def load_soundfx(path):
+    """Loads the soundfx inside the soundfx directory, a subdirectory
+    of game_data.
+    
+    Args:
+    
+        path:
+            A string representing a path that comes after the root
+            (game_data/)
+    """
+
+    return mixer.Sound(os.path.join("game_data", path))
