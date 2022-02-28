67a6fabf0662632c820f60f307a6ea8bf5bc168d
moura <m0ur4@protonmail.com>
2022-02-28 17:38:43 -0300

    All the game elements were created.

--- pong_game/game_elements/ball.py
+++ pong_game/game_elements/ball.py
@@ -1,41 +1,78 @@
-import pygame
+import pygame.sprite as sprite
+from .. import utils
 
 
-class Ball:
+class Ball(sprite.Sprite):
     """Class that represents a literal ball. This ball is used on the
     game or anything else, like a demonstration.
     """
 
     def __init__(self, screen):
+        """Initialises the Ball object.
+        
+        Args:
+        
+            screen:
+                A Surface object representing the whole window
+                background.
+        """
+        sprite.Sprite.__init__(self)
+
         self.screen = screen
         self.screen_rect = screen.get_rect()
 
-        self.x = 0
-        self.y = 0
-        self.radius = 10
-
-        self.base_colour = (255, 255, 255)
-        self.line_colour = (0, 0, 0)
+        self.image = utils.load_image("on_game/ball.png")
+        self.hit_soundfx = utils.load_soundfx("on_game/soundfx/ball_hit.ogg")
+        self.rect = self.image.get_rect()
 
         self.xspeed = 2
         self.yspeed = 2
 
+        self.points = 0
+
+    @property
+    def x(self):
+        return self.rect.x
+
+    @property
+    def y(self):
+        return self.rect.y
+
+    @x.setter
+    def x(self, value):
+        self.rect.x = value
+
+    @y.setter
+    def y(self, value):
+        self.rect.y = value
+
     def draw(self):
         """It draws the ball on the surface."""
 
-        pygame.draw.circle(self.screen, self.line_colour, (self.x, self.y),
-                           self.radius+1)
-        pygame.draw.circle(self.screen, self.base_colour, (self.x, self.y),
-                           self.radius)
+        self.screen.blit(self.image, self.rect)
 
-    # TODO: Implement ball physics here
-    def update(self):
-        """It updates the movement of the ball."""
+    def update(self, paddle):
+        """It updates the movement of the ball.
+        
+        Args:
+        
+            paddle:
+                A paddle (player) object where the rect attribute
+                will be extracted.
+        """
+
         self.x += self.xspeed
         self.y += self.yspeed
 
         # Movement logic
-        if self.x <= self.screen_rect.left or self.x >= self.screen_rect.right:
+        if self.rect.left <= self.screen_rect.left \
+                or self.rect.right >= self.screen_rect.right:
+            self.hit_soundfx.play()
             self.xspeed *= -1
-        if self.y >= self.screen_rect.bottom or self.y <= self.screen_rect.top:
+        if self.rect.top <= self.screen_rect.top:
+            self.hit_soundfx.play()
+            self.yspeed *= -1
+
+        if self.rect.colliderect(paddle.rect) and self.yspeed > 0:
+            self.hit_soundfx.play()
             self.yspeed *= -1
--- pong_game/game_elements/paddle.py
+++ pong_game/game_elements/paddle.py
@@ -1,19 +1,30 @@
-import pygame
+import pygame.sprite as sprite
+
+from ..utils import load_image
 
 
-class Paddle:
+class Paddle(sprite.Sprite):
     """Class that represents the player. Literally a white rectangle
     that hit the ball up.
     """
 
     def __init__(self, screen):
+        """Initialises the Paddle object.
+        
+        Args:
+
+            screen:
+                A Surface object representing the window
+                background.
+        """
+
+        sprite.Sprite.__init__(self)
+
         self.screen = screen
         self.screen_rect = screen.get_rect()
 
-        self.base_colour = (255, 255, 255)
-        self.line_colour = (0, 0, 0)
-
-        self.rect = pygame.Rect(0, 0, 100, 20)
+        self.image = load_image("on_game/paddle.png")
+        self.rect = self.image.get_rect()
 
         self.moving_right = False
         self.moving_left = False
@@ -22,8 +33,7 @@ class Paddle:
     def draw(self):
         """It draws the paddle on the screen."""
 
-        pygame.draw.rect(self.screen, self.line_colour, self.rect, width=4)
-        pygame.draw.rect(self.screen, self.base_colour, self.rect)
+        self.screen.blit(self.image, self.rect)
 
     def move_left(self, moving: bool):
         self.moving_left = moving
@@ -33,6 +43,7 @@ class Paddle:
 
     def update(self):
         """It updates the paddle state."""
+
         if self.moving_right and self.rect.right <= self.screen_rect.right:
             self.rect.x += self.speed
         if self.moving_left and self.rect.left >= self.screen_rect.left:
